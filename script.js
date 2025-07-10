2ede0ec075d1db04615f4f19a1b51e87ee17cbed
Okerew <93822247+Okerew@users.noreply.github.com>
2025-07-10 16:00:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+function toggleVisibility(id) {
+  var x = document.getElementById(id);
+  if (x.classList.contains("visible")) {
+    x.classList.remove("visible");
+  } else {
+    x.classList.add("visible");
+  }
+}
+function autoclicker() {
+  toggleVisibility("autoclicker");
+}
+
+function spammer() {
+  toggleVisibility("spammer");
+}
+
+function pyos() {
+  toggleVisibility("pyos");
+}
+
+function dptech() {
+  toggleVisibility("dptech");
+}
+
+function ltsm() {
+  toggleVisibility("ltsm");
+}
+
+function ok_l() {
+  toggleVisibility("ok_l");
+}
+
+function oce() {
+  toggleVisibility("oce");
+}
+function okrol() {
+  toggleVisibility("okrol");
+}
+function okterminal() {
+  toggleVisibility("okterminal");
+}
+
+function ok_blockchain() {
+  toggleVisibility("okblockchain");
+}
+function oklearn() {
+  toggleVisibility("oklearn");
+}
+function osxiec() {
+  toggleVisibility("osxiec");
+}
+
+function biobridge() {
+  toggleVisibility("biobridge");
+}
+
+function depramanager(){
+  toggleVisibility("depramanager");
+}
